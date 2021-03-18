a1a561097e27fafc031f2349f5d721d7ece2d33f
InterstellarDrift <78432610+InterstellarDrift@users.noreply.github.com>
2021-03-18 19:47:10 -0400

    Update RobotMap.java
    Corrected CAN IDs

--- src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
@@ -1,19 +1,19 @@
 package com.swervedrivespecialties.exampleswerve;
 
 public class RobotMap {
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2; // CAN
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 0; // Analog
-    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1; // CAN
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 27; // CAN
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 33; // Analog
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 26; // CAN
 
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 4; // CAN
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 1; // Analog
-    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 3; // CAN
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 21; // CAN
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 30; // Analog
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 20; // CAN
 
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 6; // CAN
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 2; // Analog
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 5; // CAN
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 25; // CAN
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 32; // Analog
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 24; // CAN
 
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 8; // CAN
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 3; // Analog
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 7; // CAN
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 23; // CAN
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 31; // Analog
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 22; // CAN
 }
