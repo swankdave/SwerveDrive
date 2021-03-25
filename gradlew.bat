4f043676451580591b04dd5fc260ebe54b030c79
Gregory Meyer <49727190+Gmeyer11@users.noreply.github.com>
2021-03-25 17:59:10 -0400

    comment
    trying to make sure 1, how this works and 2, if this will work from vs code.

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  Gradle startup script for those who have to die
 @rem
 @rem ##########################################################################
 
